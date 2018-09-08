6477d27386802e3bb3af6b75a806320421e1ea08
George Xu <33054982+georgexu99@users.noreply.github.com>
2018-09-08 13:07:18 -0700

    Add files via upload

--- popups.js
+++ popups.js
@@ -2,8 +2,8 @@ var productsListLink = document.querySelectorAll(".products-grid .item .product-
 for (var i = 0; i < productsListLink.length; i++) {
     var a = productsListLink[i];
     var name = a.innerHTML || "";
-    var addon = document.querySelector(".products-grid .item .product-name a:not(.product-image)").getAttribute('href');
-    var newaddon = addon.replace("http://tomleemusic.ca/","");
+    var addon = a.getAttribute('href');
+    var newaddon = " SKU: " + addon.replace("http://tomleemusic.ca/","");
     name += newaddon;
     a.innerHTML = name;
     a.setAttribute('title', name);
